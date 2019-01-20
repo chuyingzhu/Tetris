855d8a638fc4d6f7d49ca617557e6fcfa86fe448
chuyingzhu <45306088+chuyingzhu@users.noreply.github.com>
2019-01-20 04:34:27 -0500

    game code

---
+++ Block.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.awt.*;
+
+public enum Block {
+    OBLOCK  (new int[][] {{0,0}, {1,0}, {0,1}, {1,1}}, Color.YELLOW),
+    LBLOCK  (new int[][] {{0,-1}, {0,0}, {0,1}, {1,1}}, Color.ORANGE),
+    JBLOCK  (new int[][] {{0,-1}, {0,0}, {0,1}, {-1,1}}, Color.BLUE),
+    TBLOCK  (new int[][] {{0,-1}, {0,0}, {-1,0}, {1,0}}, Color.MAGENTA),
+    IBLOCK  (new int[][] {{0,-1}, {0,0}, {0,1}, {0,2}}, Color.CYAN),
+    SBLOCK  (new int[][] {{-1,0}, {0,0}, {0,-1}, {1,-1}}, Color.GREEN),
+    ZBLOCK  (new int[][] {{1,0}, {0,0}, {0,-1}, {-1,-1}}, Color.RED);
+
+    private int[][] points;
+    private Color color;
+
+    Block(int[][] points, Color color) {
+        this.points = points;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public int[][] getPoints() {
+        return this.points;
+    }
+}
+
+
